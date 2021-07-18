0156a5614ce5821faf1be20bd2083a88174b6a8f
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2021-07-18 15:13:25 +0700

    update: add api caching with UUID as key - set cache expiration to 1 hour

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,13 +1,43 @@
 import { Router } from 'express'
 import flickrSDK, { PublicPhotoFeed } from 'flickr-sdk'
+import { v4 } from 'uuid'
+import { client as redisClient } from '../cache/redis'
 
 const APIRouter = Router()
 
-APIRouter.get('/feed', async (req, res) => {
-  const feeds = new flickrSDK.Feeds()
-  const feedsResult = await feeds.publicPhotos({...req.query})
+/**
+ * Cache Expiration (1 hour)
+ */
+const EXPIRE_DURATION_IN_SECONDS = 3600
 
-  res.json((feedsResult.body as unknown as PublicPhotoFeed).items)
+APIRouter.get('/feed', async (req, res) => {
+  try {
+    const feeds = new flickrSDK.Feeds()
+    const { request_id, ...restQuery } = req.query
+  
+    if (request_id) {
+      const data = await new Promise<string|null>((resolve, reject) => {
+        redisClient.get(request_id as string, (err, data) => {
+          if (err) return reject(err)
+  
+          resolve(data)
+        })
+      })
+
+      return res.json({ id: request_id, data: JSON.parse(data || '') })
+    }
+
+    const id = v4()
+    const feedsResult = await feeds.publicPhotos({...restQuery})
+    const result = feedsResult.body as unknown as PublicPhotoFeed
+    redisClient.set(id, JSON.stringify(result.items))
+    redisClient.expire(id, EXPIRE_DURATION_IN_SECONDS)
+
+    res.json({ id, data: (result).items })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json(error)
+  }
 })
 
 export default APIRouter
