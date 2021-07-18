d535020c1aabab8832b9d7be2292902f298ec840
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2021-07-19 02:30:43 +0700

    update: CORS package

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import APIRouter from './routes/api'
 
 const app = express()
-    
+
+app.use(cors())
 app.use(morgan('combined'))
 app.use('/api', APIRouter)
 app.use('/', (req, res) => {
